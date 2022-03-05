5e067c58254c9c4725a088ab04c35b567ffe5b23
taka-sho0220 <ts1988220@gmail.com>
2022-03-05 13:56:20 +0900

    propTypesの概念

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
 const App = () => {
   const profiles = [
     {name: "Taro" , age: 10},
-    {}
+    {name: "test",}
   ]
   return (
     <div>
@@ -19,8 +22,13 @@ const User = (props) => {
 }
 
 User.defaultProps = {
-  name: "undefined",
-  age: 0,
+  name: 'Taro',
+  age: 0
+}
+
+User.propTypes = {
+  name: PropTypes.string.isRequired,
+  age: PropTypes.number.isRequired,
 }
 
 export default App;
