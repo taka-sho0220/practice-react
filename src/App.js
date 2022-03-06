2bbcb98b88b3b660591e4bc60a4ea97e0be58ae8
taka-sho0220 <ts1988220@gmail.com>
2022-03-06 10:23:00 +0900

    reduxのアクションファイル作成

--- src/App.js
+++ src/App.js
@@ -1,34 +1,65 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+// import React from 'react';
 
-const App = () => {
-  const profiles = [
-    {name: "Taro" , age: 10},
-    {name: "test",}
-  ]
-  return (
-    <div>
-      {
-        profiles.map((profiles,index) => {
-          return <User name={profiles.name} age={profiles.age} key={index} />
-        })
-      }
-    </div>
-  )
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+
+// const App = () => {
+//   const profiles = [
+//     {name: "Taro" , age: 10},
+//     {name: "test",}
+//   ]
+//   return (
+//     <div>
+//       {
+//         profiles.map((profiles,index) => {
+//           return <User name={profiles.name} age={profiles.age} key={index} />
+//         })
+//       }
+//     </div>
+//   )
+// }
+
+// const User = (props) => {
+//   return <div>{props.name},{props.age}</div>
+// }
+
+// User.defaultProps = {
+//   name: 'Taro',
+//   age: 0
+// }
+
+// User.propTypes = {
+//   name: PropTypes.string.isRequired,
+//   age: PropTypes.number.isRequired,
+// }
+
+
+
+const App = () => (<Counter></Counter>)
+
+class Counter extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { count: 0}
+  }
+  
+  handlePlusButton = () => {
+    this.setState({count: this.state.count + 1})
+  }
+  handleMinusButton = () => {
+    this.setState({count: this.state.count - 1})
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div>countValue : {this.state.count}</div>
+        <button onClick={this.handlePlusButton}>＋</button>
+        <button onClick={this.handleMinusButton}>-</button>
+      </React.Fragment>
+    )
+  }
 }
 
-const User = (props) => {
-  return <div>{props.name},{props.age}</div>
-}
-
-User.defaultProps = {
-  name: 'Taro',
-  age: 0
-}
-
-User.propTypes = {
-  name: PropTypes.string.isRequired,
-  age: PropTypes.number.isRequired,
-}
 
 export default App;
